c8afd8511c7c04902cda390b1f477fa09fd8db81
Diego Augusto Molina <diegoaugustomolina@gmail.com>
2024-06-22 05:02:05 -0300

    improve benchmarks usability with benchstat -col /implem

--- bytes_bench_test.go
+++ bytes_bench_test.go
@@ -10,19 +10,19 @@ func BenchmarkEncodeRune(b *testing.B) {
 		b.Run(c.name, func(b *testing.B) {
 			p := make([]byte, 4)
 
-			b.Run("stdlib", func(b *testing.B) {
+			b.Run("implem=stdlib", func(b *testing.B) {
 				for i := 0; i < b.N; i++ {
 					utf8.EncodeRune(p, c.r)
 				}
 			})
 
-			b.Run("local", func(b *testing.B) {
+			b.Run("implem=local", func(b *testing.B) {
 				for i := 0; i < b.N; i++ {
 					EncodeRune(p, c.r)
 				}
 			})
 
-			b.Run("EncodeRune4", func(b *testing.B) {
+			b.Run("implem=EncodeRune4", func(b *testing.B) {
 				for i := 0; i < b.N; i++ {
 					EncodeRune4(p, c.r)
 				}
@@ -36,13 +36,13 @@ func BenchmarkAppendRune(b *testing.B) {
 		b.Run(c.name, func(b *testing.B) {
 			var p [4]byte
 
-			b.Run("stdlib", func(b *testing.B) {
+			b.Run("implem=stdlib", func(b *testing.B) {
 				for i := 0; i < b.N; i++ {
 					utf8.AppendRune(p[:0], c.r)
 				}
 			})
 
-			b.Run("local", func(b *testing.B) {
+			b.Run("implem=local", func(b *testing.B) {
 				for i := 0; i < b.N; i++ {
 					AppendRune(p[:0], c.r)
 				}
@@ -55,7 +55,7 @@ func BenchmarkUTF8Bytes(b *testing.B) {
 	p := new([4]byte)
 	for _, c := range runeCases {
 		b.Run(c.name, func(b *testing.B) {
-			b.Run("UTF8Bytes", func(b *testing.B) {
+			b.Run("implem=UTF8Bytes", func(b *testing.B) {
 				for i := 0; i < b.N; i++ {
 					UTF8Bytes(p, c.r)
 				}
